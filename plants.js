0a20af0fb8f14eb9d438ea73043c6f394da0ca5d
Jen Nguyen <jenniferannnguyen@live.com>
2023-03-01 16:11:25 -0700

    Initial commit

---
+++ plants.js
@@ -0,0 +1,8 @@
+var get_sunshine = true;
+var ounces_of_water = 9;
+
+if (get_sunshine && ounces_of_water >= 10) {
+    console.log("Your plant lives!");
+} else {
+    console.log("Oh no, it's dead!");
+}
